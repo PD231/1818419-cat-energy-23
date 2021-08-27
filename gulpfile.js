e0787bc1fe57baf6b968a86e1f2be194e907f3f2
PD231 <paveldontsov@mail.ru>
2021-08-27 14:45:17 +0300

    8.9. Автоматизация сборки личного проекта

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,15 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const htmlmin = require('gulp-htmlmin');
+const csso = require("postcss-csso");
+const rename = require("gulp-rename");
+const terser = require("gulp-terser");
+const squoosh = require("gulp-libsquoosh");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
+
 
 // Styles
 
@@ -14,21 +23,91 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(less())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+/* HTML MIN */
+const html =() => {
+  return gulp.src('source/*.html')
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest('build'));
+}
+
+exports.html = html;
+
+/* Scripts */
+
+const navigation_script =() => {
+  return gulp.src("source/js/navigation.js")
+  .pipe(terser())
+  .pipe(rename("navigation.min.js"))
+  .pipe(gulp.dest("build/js"));
+}
+
+exports.navigation_script = navigation_script;
+
+/*Images*/
+
+const optimazeImages = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+  .pipe(squoosh())
+  .pipe(gulp.dest("build/img"));
+}
+
+exports.optimazeImages = optimazeImages;
+
+const copyImages = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+  .pipe(gulp.dest("build/img"));
+}
+
+exports.copyImages = copyImages;
+
+/* Webp */
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("build/img"))
+}
+
+exports.createWebp = createWebp;
+
+/* Copy */
+const copy = (done) => {
+  return gulp.src([
+  "source/fonts/*.{woff2,woff}",
+  "source/*.ico"
+], {
+  base: "source"})
+  .pipe(gulp.dest("build"))
+  done();
+}
+
+exports.copy = copy;
+
+/* Clean */
+
+const clean = () => {
+  return del("build");
+}
+
+exports.clean = clean;
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -43,9 +122,38 @@ exports.server = server;
 
 const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series("styles"));
+  gulp.watch("source/js/navigation.js", gulp.series("navigation_script"));
   gulp.watch("source/*.html").on("change", sync.reload);
 }
 
 exports.default = gulp.series(
-  styles, server, watcher
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    styles,
+    html,
+    navigation_script,
+    createWebp
+    ),
+    gulp.series(
+      server,
+      watcher)
 );
+
+
+/* Build */
+
+const build = gulp.series (
+  clean,
+  copy,
+  optimazeImages,
+  gulp.parallel(
+    styles,
+    html,
+    navigation_script,
+    createWebp
+    ),
+  );
+
+  exports.build = build;
